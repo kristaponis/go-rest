85488b3ebc0a005edca4006d31be5f94ac3c4c04
kristaponis <a.kristaponis@icloud.com>
2020-12-25 21:35:02 +0200

    Added BeforeCreate method to encrypt password

--- internal/app/models/user.go
+++ internal/app/models/user.go
@@ -1,8 +1,31 @@
 package models
 
+import "golang.org/x/crypto/bcrypt"
+
 // User type
 type User struct {
 	ID          string
 	Email       string
+	Password    string
 	EncPassword string
+}
+
+// BeforeCreate method
+func (u *User) BeforeCreate() error {
+	if len(u.Password) > 0 {
+		enc, err := encString(u.Password)
+		if err != nil {
+			return err
+		}
+		u.EncPassword = enc
+	}
+	return nil
+}
+
+func encString(s string) (string, error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
 }
